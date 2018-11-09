b40a39894310e1b28264c87831d1714a756fbd8d
Shawn Cicoria <github@cicoria.com>
2018-11-09 04:05:06 -0500

    scripts

---
+++ run.ps1
@@ -0,0 +1,3 @@
+
+python join_with_metadata.py ./data.tsv ./metadata.txt > ./data-with-metadata.tsv
+python make_report.py ./data-with-metadata.tsv ./report.html
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+python3 join_with_metadata.py ./data.tsv ./metadata.txt > ./data-with-metadata.tsv
+python3 make_report.py ./data-with-metadata.tsv ./report.html
